f5e4195a9542ed6bb8f8829f79d2ad00d0325c63
leandro <ljbadillo7@soy.sena.edu.co>
2023-07-25 18:56:36 -0500

    Diseño de loguin

--- lib/app/presentation/views/login/login_view.dart
+++ lib/app/presentation/views/login/login_view.dart
@@ -1,11 +1,100 @@
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
+
   static const String name = 'login_view';
+
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40.0,
+          vertical: 90.0,
+        ),
+        children: const <Widget> [
+          Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.black,
+                  backgroundImage: AssetImage('assets/images/nasa_logo.png'),
+                ),
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 50.0,
+                    color: Colors.black87,
+                  ),
+                ),
+                Divider(
+                  height: 18,
+                ),
+                TextField(
+                  enableInteractiveSelection: false,
+                  autofocus: true,
+                  textCapitalization: TextCapitalization.characters,
+                  decoration: InputDecoration(
+                    hintText: 'Name User',
+                    labelText: 'Name User',
+                    suffixIcon: Icon(
+                      Icons.account_circle
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.vertical(),
+                    )
+                  ),
+                ),
+                Divider(
+                  height: 18.0,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    labelText: 'Email',
+                    suffixIcon: Icon(Icons.alternate_email),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.vertical()
+                    )
+                  ),
+                ),
+                Divider(
+                  height: 18.0,
+                ),
+                TextField(
+                  enableInteractiveSelection: false,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: 'Password',
+                      labelText: 'Password',
+                      suffixIcon: Icon(Icons.lock_outline),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.vertical()
+                      )
+                  ),
+                ),
+                Divider(
+                  height: 15.0,
+                ),
+                SizedBox(
+                  child: FilledButton(
+                      onPressed: null,
+                      child: Text('Login in'),
+                  ),
+
+                ),
+              ],
+            ),
+
+        ],
+      )
+    );
   }
 }
